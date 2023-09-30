49b819aa8b8a95f32b8484b980b4666fecc4571d
billdeptrai0512 <billnguyen051298@gmail.com>
2023-10-01 00:59:38 +0700

    It done

--- script.js
+++ script.js
@@ -14,13 +14,9 @@ function divide(x,y) {
     return x / y;
 }
 
-let firstNumber;
-let secondNumber;
-let op;
-
 function operate(firstNumber,op,secondNumber) {
-    firstNumber = parseInt(firstNumber);
-    secondNumber = parseInt(secondNumber);
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
     if(op === `+`) {
         return add(firstNumber,secondNumber);
     }
@@ -35,14 +31,92 @@ function operate(firstNumber,op,secondNumber) {
     }
 }
 
+
+let firstNumber = "";
+let secondNumber = "";
+let op = "";
+let result = "";
+
 const numbers = document.querySelectorAll(".number");
 const display = document.querySelector(".display");
 const clear = document.querySelector(".clear")
+const operator = document.querySelectorAll(".operator");
+const dele = document.querySelector(".del");
+const dot = document.querySelector(".dot");
 
-numbers.forEach(number => number.addEventListener("click", () => {
-    display.innerHTML += number.innerHTML;
+function show(str) {
+    display.innerHTML = "";
+    display.innerHTML += str;
+    return;
+}
+
+function clearAll() {
+    firstNumber = ""
+    secondNumber = ""
+    op = ""
+    result = ""
+    display.innerHTML = ""
+    return;
+}
+
+function del() { 
+    display.innerHTML = display.innerHTML.slice(0, -1); 
+    if(op === "") {
+        firstNumber = firstNumber.slice(0,-1);
+    } else {
+        secondNumber = secondNumber.slice(0,-1);
+    }
+    return;
+}
+
+function decimal() {
+    if(display.innerHTML.includes(".")) return;
+    if (op === "") {
+        firstNumber += ".";
+        show(firstNumber);
+    } else {
+        secondNumber += ".";
+        show(secondNumber);
+    }
+    return
+}
+
+
+numbers.forEach(number => number.addEventListener("click", (e) => {  
+    if (op === "") {
+        firstNumber += e.target.innerHTML;
+        show(firstNumber);
+    } else {
+        if (result !== "") {
+            firstNumber = result;  
+            secondNumber += e.target.innerHTML;
+            show(secondNumber);
+        } else {
+            secondNumber += e.target.innerHTML;
+            show(secondNumber);
+        }
+    }
 }))
 
-clear.addEventListener("click", () => {
-    display.innerHTML = ""
-});
+operator.forEach(item => item.addEventListener("click", (e) => {
+    if (e.target.innerHTML !== "=") {
+        if (secondNumber !== "") {
+            op = e.target.innerHTML;
+            result = operate(firstNumber, op, secondNumber);
+            show(result);
+            secondNumber = "";
+        } else {
+            op = e.target.innerHTML;
+        }
+    } else {
+        result = operate(firstNumber,op,secondNumber);
+        show(result);
+        secondNumber = "";
+    }
+}))
+
+clear.addEventListener('click', clearAll);
+
+dele.addEventListener('click', del);
+
+dot.addEventListener('click', decimal);
